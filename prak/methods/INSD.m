fc007b2a5563a59c60f185e008579fc6921e1be5
Vasyka <vvanast@yandex.ru>
2018-07-22 02:01:39 +0300

    Calculating sums+comments

--- prak/methods/INSD.m
+++ prak/methods/INSD.m
@@ -1,4 +1,20 @@
 function result = INSD (Az, u, v, epsilon = 0.01, maxim = 5000)
+% PURPOSE: estimate a new matrix X on the base of X0 with exogenously
+% given row and column totals.
+% -------------------------------------------------------------------------
+% USAGE: X = INSD(X0,u,v) with or without epsilon and maxim
+% included as the five argument, where
+% INPUT:
+% -> Az = benchmark (base) matrix, not necessarily square
+% -> u = row vector of row totals
+% -> v = column vector of column totals
+% -> epsilon = convergence tolerance level; if empty, the default threshold
+% is 0.01
+% -> maxim = maximum itterations before algorithm stops, if empty, the default
+% limit is 10.000 itterations
+% OUTPUT:
+% -> result = estimated/adjusted/updated matrix
+
   itt = 0;
   Az(Az == 0) = 0.01;
 
@@ -23,10 +39,8 @@ function result = INSD (Az, u, v, epsilon = 0.01, maxim = 5000)
     # Prevent negative numbers
     neg = M * Az .* min(0, Z);
     
-    #Lambdas
+    # Coefficients of the differences between real sums of rows/columns and estimated
     lambd = (u - u0 + sum(neg - tau .* Az, 2)) ./ u0;
-    
-    #Taus
     tau = (v - v0 + sum(neg - lambd .* Az, 1)) ./ v0;
     
     #Z
@@ -34,6 +48,7 @@ function result = INSD (Az, u, v, epsilon = 0.01, maxim = 5000)
     Z(Az == 0) = 1;
     Z = ifelse(Z >= 0, Z, Z / (1 + M));
     
+    # Calculate residual between coefficients of the columns and rows sums differenses
     diffLambd = abs(lambd .- lambd0);
     diffTau = abs(tau .- tau0);
     itt += 1;
@@ -41,8 +56,9 @@ function result = INSD (Az, u, v, epsilon = 0.01, maxim = 5000)
   until(or(
         and(all(diffLambd < epsilon),
             all(diffTau < epsilon)),
-        itt > maxim))
+        itt > maxim)) # working until both columns and rows coefficients are less than epsilon, or limit is reached
   
+  # Get estimated matrix 
   X = Z .* Az;
-  result = round(X);
+  result = X;
 endfunction
